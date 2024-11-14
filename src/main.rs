e9cf2b3794a6238c62828282f06088b8ad793c98
Davi Oliveira <davigbso@hotmail.com>
2024-11-13 22:11:58 -0300

    First version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use image::GenericImageView;
+
+fn get_str_ascii(intent: u8) -> &'static str {
+    let index = intent / 32;
+    let ascii = [" ", "/", "|", "-", "4", "5", "6", "7"];
+    // let ascii = [" ", ".", ",", "-", "~", "+", "=", "@"];
+    return ascii[index as usize];
+}
+
+fn get_image(dir: &str, scale: u32) {
+    let img = image::open(dir).unwrap();
+    println!("{:?}", img.dimensions());
+    let (width, height) = img.dimensions();
+    for y in 0..height {
+        for x in 0..width {
+            if y % (scale * 2) == 0 && x % scale == 0 {
+                let pix = img.get_pixel(x, y);
+                let mut intent = pix[0] / 3 + pix[1] / 3 + pix[2] / 3;
+                if pix[3] == 0 {
+                    intent = 0;
+                }
+                print!("{}", get_str_ascii(intent));
+            }
+        }
+        if y % (scale * 2) == 0 {
+            println!("");
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    get_image("assets/Silverback.jpg", 4);
 }
